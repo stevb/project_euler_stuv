686548b7ca420dcfe97d182421d7c44a9f8e0d71
steveb <stevebrust@gmail.com>
2016-09-24 14:26:04 +0200

    major speedup after some analysis and pattern searching

--- proj_euler69.py
+++ proj_euler69.py
@@ -1,38 +1,10 @@
-#proj_euler69.py
-import numpy as np
-from fractions import gcd
-
-def isprime(number):
-    prime = False
-    if number > 1:
-        prime = True
-        q = 2
-        red =  number**0.5 
-        while q <= red and prime == True:
-            if number % q == 0:
-                prime = False
-            q += 1
-    return prime
-
-to_val = 1e6   
-primes_list = []
-global primes_list
-for i in range(2,int(to_val)):
-    if isprime(i) == True:
-        primes_list.append(i) 
-def totient(n, the_list):
-    #wikipedia Euler's Tolient function
-    #I like Python's list comprehension
-    #p are distinct prime factors
-    phi = [(1-1./p) for p in the_list if n%p == 0]
-    return n*np.prod(phi)    
-phi_list = []
-sol_list = []
-vals = []
-for n in range(2,int(to_val)):
-    vals.append(n)
-    phi_list.append(totient(vals[-1], [x for x in primes_list if x<=(vals[-1])]))
-    sol_list.append((vals[-1])/phi_list[-1])
-print "winner: ", vals[sol_list.index(max(sol_list))] 
-print "phi is: ", phi_list[sol_list.index(max(sol_list))] 
-print "n/phi is: ", sol_list[sol_list.index(max(sol_list))] 
+#Project Euler Problem 69
+#looking at some patters on Wiki; we need to find the phi that is produced as a result of products of primes because this will result in a maximum n and a minimum phi. The phis for primes are tiny.
+#these are the prime numbers whose product is greater than 1e6; they're the only ones we'll need. The code will print the result before 19 is used because the product that uses all primes is greater than 1e6
+primes = [2, 3, 5, 7, 11, 13, 17, 19]
+result = 1
+upper_lim = int(1e6)
+for p in primes:
+    if p*result > upper_lim:
+        print "the max. n/phi occurs for n = ", result
+    result *= p
